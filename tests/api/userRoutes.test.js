df16408e574db8e6d9ff93d6c87f140670538400
michael dinh <mkdinh94@gmail.com>
2018-05-14 10:46:23 -0400

    remove async on non request test

--- tests/api/userRoutes.test.js
+++ tests/api/userRoutes.test.js
@@ -17,13 +17,13 @@ describe("User Routes", () => {
       const res = await request.login();
       user = request._user;
     });
-    test("GET /users/:id, expect to get back user info", async () => {
+    test("GET /users/:id, expect to get back user info", () => {
       expect(true).toEqual(true);
     });
     //   test("POST /users/", async () => {});
   });
-  describe("when user not logged in", () => {
-    test("GET /users/:id, expect to return 401 error", async () => {});
-    test("POST /users/, expect to return 401 error", async () => {});
-  });
+  // describe("when user not logged in", () => {
+  //   test("GET /users/:id, expect to return 401 error", async () => {});
+  //   test("POST /users/, expect to return 401 error", async () => {});
+  // });
 });
